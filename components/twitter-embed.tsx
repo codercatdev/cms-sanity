cf5324de057eb4a07477ad14162639d3f8197c43
Alex Patterson <alex@codingcat.dev>
2024-06-18 18:54:36 -0400

    correct twitterembed comp name

--- components/twitter-embed.tsx
+++ components/twitter-embed.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { TwitterTweetEmbed } from "react-twitter-embed";
 
-export default function TwitterPreview(props: any) {
+export default function TwitterEmbed(props: any) {
   const { id } = props;
   if (!id) {
     return <div>Add Twitter (X) id</div>;
